612ae10d83d9997f58305bc874b51a17e3e7ef37
craigs40 <craig.shay40@gmail.com>
2021-03-10 15:46:56 -0500

    Remove class Gameplay based on console error

--- bin/main.rb
+++ bin/main.rb
@@ -3,94 +3,91 @@
 require_relative '../lib/board'
 require_relative '../lib/player'
 
-# flow of the game
-class Gameplay
-  def display(board)
-    puts "\n#{board[0]} | #{board[1]} | #{board[2]}"
-    puts '---------'
-    puts "#{board[3]} | #{board[4]} | #{board[5]}"
-    puts '---------'
-    puts "#{board[6]} | #{board[7]} | #{board[8]}"
+def display(board)
+  puts "\n#{board[0]} | #{board[1]} | #{board[2]}"
+  puts '---------'
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts '---------'
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+end
+board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+display(board)
+
+puts "\nWelcome to Tic Tac Toe!"
+
+puts "\nPlayer 1, what is your name?"
+player1 = gets.chomp.capitalize
+while player1.empty?
+  puts 'Error! Please enter your name...'
+  player1 = gets.chomp.capitalize
+end
+
+puts "\nPlayer 2, what is your name?"
+player2 = gets.chomp.capitalize
+while player2.empty?
+  puts 'Error! Please enter your name...'
+  player2 = gets.chomp.capitalize
+end
+
+puts "\nHello, #{player1} and #{player2}!"
+puts "\n#{player1} will be X, and #{player2} will be O."
+
+puts "\nLet's begin!"
+
+game_on = true
+turn = 1
+
+while game_on
+  if turn >= 5
+    puts "\nDraw!"
+    break
   end
-  board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+  turn += 1
+
+  puts "\n#{player1}, please select an available cell from the board..."
+  player1move = gets.chomp.to_i
+  symbol1 = 'x'
+  p1_array << player1move
+  until player1move.between?(0, 8)
+    puts 'Invalid move! Please select a number between 0 and 8...'
+    player1move = gets.chomp.to_i
+  end
+  puts "\n#{player1} selected #{player1move}." if player1move.between?(0, 8)
+  if player1move == player2move
+    puts 'Cell taken! Please select an available cell from the board...'
+    player1move = gets.chomp.to_i
+    puts "\nYou've selected #{player1move}"
+  end
+
+  board[player1move] = symbol1
   display(board)
 
-  puts "\nWelcome to Tic Tac Toe!"
-
-  puts "\nPlayer 1, what is your name?"
-  player1 = gets.chomp.capitalize
-  while player1.empty?
-    puts 'Error! Please enter your name...'
-    player1 = gets.chomp.capitalize
+  if WINNING_COMBOS.include? p1_array
+    puts "\n#{player1} WINS!"
+    break
   end
 
-  puts "\nPlayer 2, what is your name?"
-  player2 = gets.chomp.capitalize
-  while player2.empty?
-    puts 'Error! Please enter your name...'
-    player2 = gets.chomp.capitalize
-  end
-
-  puts "\nHello, #{player1} and #{player2}!"
-  puts "\n#{player1} will be X, and #{player2} will be O."
-
-  puts "\nLet's begin!"
-
-  game_on = true
-  turn = 1
-
-  while game_on
-    if turn >= 5
-      puts "\nDraw!"
-      break
-    end
-
-    turn += 1
-
-    puts "\n#{player1}, please select an available cell from the board..."
-    player1move = gets.chomp.to_i
-    symbol1 = 'x'
-    p1_array << player1move
-    until player1move.between?(0, 8)
-      puts 'Invalid move! Please select a number between 0 and 8...'
-      player1move = gets.chomp.to_i
-    end
-    puts "\n#{player1} selected #{player1move}." if player1move.between?(0, 8)
-    if player1move == player2move
-      puts 'Cell taken! Please select an available cell from the board...'
-      player1move = gets.chomp.to_i
-      puts "\nYou've selected #{player1move}"
-    end
-
-    board[player1move] = symbol1
-    display(board)
-
-    if WINNING_COMBOS.include? p1_array
-      puts "\n#{player1} WINS!"
-      break
-    end
-
-    puts "\n#{player2}, please select an available cell from the board..."
+  puts "\n#{player2}, please select an available cell from the board..."
+  player2move = gets.chomp.to_i
+  symbol2 = 'o'
+  p2_array << player2move
+  until player2move.between?(0, 8)
+    puts 'Invalid move! Please select a number between 0 and 8...'
     player2move = gets.chomp.to_i
-    symbol2 = 'o'
-    p2_array << player2move
-    until player2move.between?(0, 8)
-      puts 'Invalid move! Please select a number between 0 and 8...'
-      player2move = gets.chomp.to_i
-    end
-    puts "\n#{player1} selected #{player1move}." if player2move.between?(0, 8)
-    if player2move == player1move
-      puts 'Cell taken! Please select an available cell from the board...'
-      player2move = gets.chomp.to_i
-      puts "\nYou've selected #{player2move}"
-    end
+  end
+  puts "\n#{player1} selected #{player1move}." if player2move.between?(0, 8)
+  if player2move == player1move
+    puts 'Cell taken! Please select an available cell from the board...'
+    player2move = gets.chomp.to_i
+    puts "\nYou've selected #{player2move}"
+  end
 
-    board[player2move] = symbol2
-    display(board)
+  board[player2move] = symbol2
+  display(board)
 
-    if WINNING_COMBOS.include? p2_array
-      puts "\n#{player2} WINS!"
-      break
-    end
+  if WINNING_COMBOS.include? p2_array
+    puts "\n#{player2} WINS!"
+    break
   end
 end
